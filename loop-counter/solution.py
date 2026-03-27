8f2f8a507146793af0118928e88a3c59f22ca9b4
Liuklian Anna <liuklian.pn@ucu.edu.ua>
2026-03-27 21:43:36 +0200

    Add solution: Linked Lists - Can you get the loop?

---
+++ loop-counter/solution.py
@@ -0,0 +1,16 @@
+def loop_size(node):
+    slow = node
+    fast = node
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+
+    count = 1
+    fast = fast.next
+    while fast != slow:
+        count += 1
+        fast = fast.next
+
+    return count
